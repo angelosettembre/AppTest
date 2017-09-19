c0ff521f00b5b8a5ab86ddd66b2951602eb97508
Angelo Settembre <a.settembre8@studenti.unisa.it>
2017-09-19 14:58:04 +0200

    First release CheckPermissions

--- app/src/main/java/com/example/angiopasqui/checkpermissions/App.java
+++ app/src/main/java/com/example/angiopasqui/checkpermissions/App.java
@@ -2,21 +2,33 @@ package com.example.angiopasqui.checkpermissions;
 
 import android.graphics.drawable.Drawable;
 
+import java.io.Serializable;
+
 /**
  * Created by Angiopasqui on 18/09/2017.
  */
 
-public class App {
+public class App implements Serializable {
     private String name;
+    private String packageName;
     private Drawable icon;
 
     public App(){
 
     }
 
-    public App(String name, Drawable icon){
+    public App(String name, Drawable icon, String packageName){
         this.name = name;
         this.icon = icon;
+        this.packageName = packageName;
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
     }
 
     public String getName() {
--- app/src/main/java/com/example/angiopasqui/checkpermissions/MainActivity.java
+++ app/src/main/java/com/example/angiopasqui/checkpermissions/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.angiopasqui.checkpermissions;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Debug;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,14 +22,13 @@ import java.util.logging.Logger;
 public class MainActivity extends Activity {
     public ListView listView;
     CustomAdapter customAdapter;
+    App app;
+    String packageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        PackageManager pm = getPackageManager();
-        List<ApplicationInfo> apps = pm.getInstalledApplications(0);
 
         listView = (ListView) findViewById(R.id.listApp);
 
@@ -31,15 +36,43 @@ public class MainActivity extends Activity {
 
         listView.setAdapter(customAdapter);
 
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> apps = pm.getInstalledApplications(0);
+
+        //GET APPS
         for (ApplicationInfo applicationInfo : apps)
         {
             if((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 1) {
-                App a = new App();
-                a.setName((String) pm.getApplicationLabel(applicationInfo));
-                Log.d("DEBUG", "NOME APP" + a.getName());
-                a.setIcon(pm.getApplicationIcon(applicationInfo));
-                customAdapter.add(a);
+                app = new App();
+                app.setName((String) pm.getApplicationLabel(applicationInfo));
+                Log.d("DEBUG", "NOME APP" + app.getName());
+                app.setIcon(pm.getApplicationIcon(applicationInfo));
+                app.setPackageName(applicationInfo.packageName);
+                customAdapter.add(app);
             }
         }
+
+        /*List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        for (ApplicationInfo info : packages) {
+            Log.i("Info", "Installed package:" + info.packageName);
+        }*/
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                App a = customAdapter.getItem(position);
+                Intent i = new Intent(getApplicationContext(), AppDetail.class);
+                i.putExtra("Nome app", a.getName());
+                Log.d("DEBUG","COSSSSAA"+a.getName());
+                Drawable s = a.getIcon();
+                Bitmap bitmap = ((BitmapDrawable)s).getBitmap();
+                i.putExtra("Icona app", bitmap);
+                Log.d("DEBUG","SDASDS"+bitmap);
+                i.putExtra("PACKAGE",a.getPackageName());
+                Log.d("DEBUG","Pacchetto"+a.getPackageName());
+                startActivity(i);
+            }
+        });
     }
 }
